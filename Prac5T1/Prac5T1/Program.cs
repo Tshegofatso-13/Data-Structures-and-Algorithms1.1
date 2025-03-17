f8e831a0c2bc4f8e20db09c98038d6f03a664558
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:22:54 +0200

    Add files via upload
    Recursive Divisor Multiplication in C# 
    This C# program attempts to recursively find and multiply all divisors of a given number. 
    
    Features:
    Recursive Function (GetDiv): Supposed to multiply all divisors of a number.
    Basic Console Output: Displays "Hello World!" as a placeholder.

---
+++ Prac5T1/Prac5T1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Prac5T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            GetDiv(4, 4);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        
+        public static int GetDiv(int num, int i)
+        {
+
+            if(i==1 )
+                return 1;
+            if (num % i == 0)
+                return num * GetDiv(num,i - 1);
+            else
+            return GetDiv(num,i - 1);
+            
+        }
+
+    }
+}
