3eda8b632b15f35e4e0fbb71cf360ce79814331b
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:58:01 +0200

    Add files via upload
    This C# program simulates the movement of a robot on a 21x21 grid, following directions from an "Instructions.txt" file. It tracks the robot's path and determines the most frequently visited cell.
    
    Overview
    Uses a 2D array (inputMatrix) to represent the grid.
    Reads movement instructions (North, South, East, West) from "Instructions.txt".
    Updates the grid based on movement, counting the number of times each cell is visited.
    Displays the robot’s path as * (visited cells) and + (unvisited cells).
    Identifies and prints the most visited cell.
    Key Components
    1)Movement Processing
    
    The robot starts at the center (11,11).
    Moves North (↑), South (↓), East (→), or West (←) based on input.
    If movement reaches the grid boundary, it stays in place.
    Increments the visit count for each cell.
    2) Grid Visualisation
    
    Prints a 21x21 matrix.
    * marks visited cells, and + marks unvisited cells.
    3) Finding the Most Visited Cell
    
    Iterates through inputMatrix to find the highest visit count.
    Prints the cell's coordinates and visit frequency.

---
+++ Prac9T3/Prac9T3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Prac9T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int colLength = 21; int rowLength = 21;
+
+            double[,] inputMatrix = new double[colLength, rowLength];
+
+            int row = 11, col = 11, maxVisit = int.MinValue, rowCount = 0, colCount = 0;
+
+            StreamReader sr = new StreamReader("Instructions.txt");
+            String line = sr.ReadLine();
+
+            
+
+            while (line != null)
+            {
+                if (line.Equals("West"))
+                {
+                    if (row == 0) 
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                    }
+                    else
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                        row--;
+                    }
+                }
+
+                else
+                    if (line.Equals("East"))
+                {
+                    if (row == 20) 
+                    {
+
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                    }
+                    else
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                        row++;
+                    }
+                }
+
+
+                else
+                    if (line.Equals("North"))
+                {
+                    if (col == 0) 
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                    }
+                    else
+
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                        col--;
+
+                    }
+                }
+
+                if (line.Equals("South"))
+                {
+                    if (col == 20) 
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                    }
+                    else
+                    {
+                        inputMatrix[col, row] = inputMatrix[col, row] + 1;
+                        col++;
+                    }
+                }
+
+
+                line = sr.ReadLine();
+            }
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Robo matrix path starts");
+            Console.WriteLine("---------------------------------------------------------");
+            for (int i = 0; i < colLength; i++)
+            {
+                for (int j = 0; j < rowLength; j++)
+                {
+                    if (inputMatrix[i, j] > 0)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                        Console.Write("+");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < colLength; i++)
+            {
+                for (int j = 0; j < rowLength; j++)
+                {
+                    if (inputMatrix[i, j] > maxVisit)
+                    {
+                        maxVisit = (int)inputMatrix[i,j];
+                        rowCount = j;
+                        colCount = i;
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Robo matrix path ends");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine($"cell most frequented is ({colCount},{rowCount}) which is visited {maxVisit} times");
+            Console.ReadLine();
+        }
+        
+    }
+
+    class Node
+    {
+        public String Cargo;
+        public double CellCount;
+    }
+}
+
