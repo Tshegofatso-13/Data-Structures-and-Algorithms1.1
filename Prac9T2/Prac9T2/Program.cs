954308e308035676327e351f1b795ed716f03d94
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:46:37 +0200

    Add files via upload
    Finding Common Elements in Two Sorted Files (C#)
    This C# program reads two sorted files (File1.txt and File2.txt) and prints the common numbers between them.
    
    Features:
    - Reads sorted numerical values from two files (File1.txt, File2.txt).
    
    - Finds and prints common elements between the two files in sorted order.
    
    - Uses two pointers (curFirst and curSecond) to compare values efficiently.
    
    - Avoids storing large datasets in memory, making it efficient for large files.
    
    - Handles file reading with StreamReader to process data line by line.

---
+++ Prac9T2/Prac9T2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace Prac9T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            double curFirst, curSecond;
+            int PosFirstList = 0, PosSecondList = 0;
+
+            StreamReader sr1 = new StreamReader("File1.txt");
+            double line1 = double.Parse(sr1.ReadLine());
+
+            StreamReader sr2 = new StreamReader("File2.txt");
+            double line2 = double.Parse(sr2.ReadLine());
+
+            curFirst = line1;
+            curSecond = line2;
+
+            while (!sr1.EndOfStream && !sr2.EndOfStream)
+            {
+                
+                if (curFirst == curSecond)
+                {
+                    Console.Write($" {curFirst} ");
+                    
+                    line1 = double.Parse(sr1.ReadLine());
+                    line2 = double.Parse(sr2.ReadLine());
+                    curFirst = line1;
+                    curSecond = line2;
+                    PosFirstList++;
+                    PosSecondList++;
+
+                }
+
+                else
+                    if (curFirst < curSecond)
+                {
+                    PosFirstList++;
+                    line1 = double.Parse(sr1.ReadLine());
+                    curFirst = line1;
+                }
+                else
+                {
+                    line2 = double.Parse(sr2.ReadLine());
+                    curSecond = line2;
+                }
+
+            }
+            
+            Console.ReadLine();
+            
+
+        }
+    }
+
+}
+
