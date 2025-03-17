43ef97e6c7d2c6ece776f1117a41af5de4cf711c
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:18:40 +0200

    Add files via upload
    Recursive Minimum Finder in C#
    This C# program attempts to find the minimum value in a linked list using recursion. 
    
    Features:
    Recursive Function (MinRecurssion): Aims to find the smallest value in a linked list.
    Basic Linked List Implementation: Includes a Node class to represent linked list nodes.
    Console Output: Prints "Hello World!" as a placeholder.

---
+++ Prac4T3/Prac4T3/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Prac4T3
+{
+    public class Node
+    {
+        int data; Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
---
+++ Prac4T3/Prac4T3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace Prac4T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        public static int MinRecurssion(Node head, int min)
+        {
+            min = 9999999;
+            
+            if(head == null)
+            {
+                return min;
+            }
+            if(head.next < min)
+            {
+                min = head.data;
+            }
+            return MinRecurssion(head.next, min);
+        }
+    }
+    public class Node
+    {
+        int data; Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+}
