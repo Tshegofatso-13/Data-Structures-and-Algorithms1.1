5a19fb4ed11423e670727ee0249dc99bb4c00dac
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:12:48 +0200

    Add files via upload
    Recursive Sum Calculator in C#
    This C# program calculates the recursive sum of a given number, adjusting for even or odd values. If the user enters an odd number, the program reduces it by one to ensure an even number before performing the summation.
    
    Features:
    Recursive Function (GetSum): Computes the sum of all integers from the given number down to zero.
    Odd Number Handling: If the input is odd, it is reduced by one before summation.
    Console Interaction: Prompts the user for input and displays the evaluated sum.
    How It Works:
    The user inputs a number.
    If the number is even, the sum is calculated directly.
    If the number is odd, it is decremented by 1 before summation.
    The sum is computed recursively and displayed.

---
+++ Prac4T1/Prac4T1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Prac4T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter input number :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int evalNum;
+
+            if (num2 % 2 == 0)
+                evalNum = GetSum(num2);
+            else
+                evalNum = GetSum(num2 - 1);
+
+            Console.WriteLine($"Evaluated sum is {evalNum}");
+            Console.ReadLine();
+        }
+
+        private static int GetSum(int num)
+        {
+            if (num == 0)
+                return 0;
+
+                return num + GetSum(num - 1);
+
+        }
+    }
+}
