4c9b303a39302e87f89a8028bf5cde54ec1d9695
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:25:57 +0200

    Add files via upload
    Prime Number Checker in C#
    This C# program attempts to identify and print prime numbers within a given range using recursion.
    
    Features:
    Recursive Prime Checking (CheckForPrimes)
    Supposed to determine if a number n is prime by checking divisibility from m = 2 onward.
    Iterative Prime Printing (PrintPrimes)
    Calls CheckForPrimes for each number up to num and prints primes.

---
+++ Prac5T2/Prac5T2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prac5T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int CheckForPrimes(int m, int n)
+        {
+            if (n == m)
+                return 0;
+            if (n % m == 0)
+                return 1;
+            else
+                return CheckForPrimes(m + 1, n);
+        }
+        public static void PrintPrimes(int num)
+        {
+            Console.WriteLine($"Primes numbers between 1 and {num} are");
+
+            for(int x=2;x<=num;x++)
+            {
+                if (CheckForPrimes(2, x) == 0)
+                    Console.WriteLine(" {0} ",x);
+            }
+        }
+    }
+}
