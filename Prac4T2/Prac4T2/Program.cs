e853ec16a852dd55ecb478e0434cc66e60cdef1a
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:15:30 +0200

    Add files via upload
    This C# program attempts to approximate the value of Pi using a recursive method. 
    
    Features:
    Recursive Function (GetPi): Aims to approximate Pi using a mathematical series.
    Console Output: Prints "Hello World!" as a placeholder for testing.
    Basic Structure for a Pi Approximation Algorithm.

---
+++ Prac4T2/Prac4T2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prac4T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        private static double GetPi(int num)
+        {
+            double sum = 0;
+
+            if (sum == 0 )
+                return 1;
+            if( num % 2 != 0)
+            {
+                sum += GetPi(4 / num) - GetPi(4 / num + 2);
+                num += 2;
+            }
+            else
+            {
+                sum += GetPi(4 / num) + GetPi(4 / num + 2);
+                num += 2;
+            }
+            return sum;
+        }
+    }
+}
