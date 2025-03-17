cc6d777eeae978cc318699eaf2acbcb974a45af2
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:02:04 +0200

    Add files via upload
    This project provides a simple implementation of a Singly Linked List in C#. It reads data from a file (Dictionary.txt), inserts it into the linked list using two different methods (Insert at First and Insert at Last), and measures the time taken for each approach.
    
    Features
    Linked List Operations:
    
    Insert at the beginning
    Insert at the end
    Insert at a specific position
    Retrieve first, last, or specific elements
    Display all elements
    Search for an element
    Performance Measurement:
    
    Tracks execution time for inserting elements from a file at the beginning vs. the end.
    How It Works
    The Main function initializes a SinglyLinkedList and loads data from Dictionary.txt.
    LoadLLFirst() reads the file and inserts each line at the beginning of the list.
    LoadLLLast() reads the file and inserts each line at the end of the list.
    The execution time for each operation is measured and printed.

---
+++ Prac2T1/Prac2T1/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Prac2T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+            Console.ReadLine();
+        }
+
+        public Program()
+        {
+            SinglyLinkedList LL = new SinglyLinkedList();
+            LL.LoadLLFirst();
+            LL.LoadLLLast();
+            Console.ReadLine();
+
+           
+        }
+    }
+
+   public class SinglyLinkedList
+    {
+        private Node head;
+
+        public SinglyLinkedList()
+        {
+            head = null;
+        }
+
+        public SinglyLinkedList Convert2LL(ArrayList al)
+        {
+            SinglyLinkedList ll = new SinglyLinkedList();
+
+            for(int x = 0; x < al.Count;x++)
+            {
+                
+                ll.InsertAtFirst((String)al[x]);
+            }
+            return ll;
+        }
+        public SinglyLinkedList Convert2LL2(ArrayList al)
+        {
+            SinglyLinkedList ll = new SinglyLinkedList();
+
+            for (int x = 0; x < al.Count; x++)
+            {
+
+                ll.InsertAtLast((String)al[x]);
+            }
+            return ll;
+        }
+
+        public void LoadLLFirst()
+        {
+            long start = DateTime.Now.Ticks;
+
+            ArrayList al = new ArrayList();
+            
+            StreamReader sr = new StreamReader("Dictionary.txt");
+            String line = sr.ReadLine();
+
+            while (line != null)
+            {
+               al.Add(line);
+               line = sr.ReadLine();
+                   
+            }
+
+            sr.Close();
+            Convert2LL(al);
+            long end = DateTime.Now.Ticks;
+
+            Console.WriteLine("File reading time for InsertFirst: " + (end - start) / 10000);
+            Console.WriteLine("Done");
+        }
+
+        public void LoadLLLast()
+        {
+            long start = DateTime.Now.Ticks;
+
+            ArrayList al = new ArrayList();
+            
+            StreamReader sr = new StreamReader("Dictionary.txt");
+            String line = sr.ReadLine();
+
+            while( line != null)
+            {
+                al.Add(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Convert2LL2(al);
+
+            long end = DateTime.Now.Ticks;
+
+            Console.WriteLine("File reading time for InsertLast: " + (end - start) / 10000);
+            
+        }
+
+        public void DisplayAll()
+        {
+            Console.WriteLine("Display List");
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.Cargo);
+                temp = temp.next;
+            }
+        }
+
+        public String GetFirst()
+        {
+            if (head != null) return head.Cargo;
+            else return null;
+        }
+
+        public String GetLast()
+        {
+            if (head == null) return null;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                return temp.Cargo;
+            }
+        }
+
+        public String GetAt(int pos)
+        {
+            if (head == null) return null;
+            else
+            {
+                Node temp = head;
+                while (pos > 0)
+                {
+                    pos--;
+                    temp = temp.next;
+                    if (temp == null) return null;
+                }
+
+                return temp.Cargo;
+            }
+        }
+
+        public void InsertAtFirst(String cargo)
+        {
+            Node newnode = new Node();
+            newnode.Cargo = cargo;
+            newnode.next = head;
+            head = newnode;
+        }
+
+        public void InsertAtLast(String cargo)
+        {
+            Node newnode = new Node();
+            newnode.Cargo = cargo;
+
+            if (head == null) head = newnode;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = newnode;
+            }
+        }
+
+        public bool InsertAt(int pos, String cargo)
+        {
+            Node newnode = new Node();
+            newnode.Cargo = cargo;
+
+            if (pos == 0)
+            {
+                newnode.next = head;
+                head = newnode;
+                return true;
+            }
+            else
+            {
+                if (head == null) return false;
+                pos--; // We want to find the node before the insert spot
+                Node temp = head;
+                while (pos > 0)
+                {
+                    pos--;
+                    temp = temp.next;
+                    if (temp == null) return false;
+                }
+
+                newnode.next = temp.next;
+                temp.next = newnode;
+                return true;
+            }
+        }
+
+        public String Search(String cargo)
+        {
+            Node temp = head;
+            while ((temp != null) && (temp.Cargo != cargo))
+            {
+                temp = temp.next;
+            }
+
+            if (temp != null) return temp.Cargo;
+            else return null;
+        }
+
+
+
+    }
+
+    class Node
+    {
+        public String Cargo;  //This linked list stores Strings
+        public Node next;  //Reference to next list element
+    }
+}
