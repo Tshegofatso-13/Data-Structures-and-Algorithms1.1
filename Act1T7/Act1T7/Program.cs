a86d43ca20a186de70b093e669182b82d1bc6fb9
Tshegofatso-13 <56923380+Tshegofatso-13@users.noreply.github.com>
2025-03-17 16:31:28 +0200

    Add files via upload
    Currency Cent Tracker in C#
    This C# program collects and analyzes monetary values (in cents) using an array. It provides basic operations such as calculating the total sum, counting values below a threshold, and finding the minimum value.
    
    Features:
    User Input Collection (getArrayInput)
    Stores up to 10 valid cent values (1 to 500 cents).
    Total Sum Calculation (getTotal)
    Displays the sum of all values in currency format.
    Count of Values Below R1.00 (getCount)
    Counts values less than 100 cents and displays the count.
    Find Minimum Value (getMin)
    Finds and returns the smallest cent value in the array.

---
+++ Act1T7/Act1T7/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Act1T7
+{
+   
+    class Program
+    {
+        static int[] cents = new int[10];
+        static void Main(string[] args)
+        {
+            Console.Write("hu");
+            getArrayInput();
+        }
+        public static void getArrayInput()
+        {
+            for(int i = 0; i <=cents.Length-1;i++)
+            {
+                Console.WriteLine("Enter cents :");
+                int input = int.Parse(Console.ReadLine());
+
+                if(input > 0 && input <= 500)
+                {
+                    cents[i] = input;
+                }
+            }
+        }
+        public static void getTotal()
+        {
+            double sum = 0;
+
+            for(int i=0;i<=cents.Length-1;i++)
+            {
+                sum += cents[i];
+            }
+
+            Console.WriteLine("The total sum is {0:C2}", sum);
+        }
+        public static void getCount()
+        {
+            int count = 0;
+            for(int i=0;i<=cents.Length-1;i++)
+            {
+                if(cents[i] < 1.00)
+                {
+                    count++;
+                    
+                }
+            }
+            Console.WriteLine($"There are {count} elements with currency less than R1.00 ");
+        }
+        public static int getMin()
+        {
+            int minVal = int.MaxValue;
+
+            for (int i = 0; i <= cents.Length - 1; i++)
+            {
+                if(cents[i] < minVal)
+                {
+                    minVal = cents[i];
+                }
+            }
+
+            return minVal;
+        }
+    }
+}
